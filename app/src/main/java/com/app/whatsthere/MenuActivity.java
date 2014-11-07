8bde4a341847f064549b9b9786787f1dc0423422
Marat <imarat@cambium.co.il>
2014-11-07 15:50:19 +0200

    adding menu actvity

--- app/src/main/java/com/app/whatsthere/MenuActivity.java
+++ app/src/main/java/com/app/whatsthere/MenuActivity.java
@@ -4,24 +4,36 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.facebook.*;
-import com.facebook.Session;
-import android.content.Intent;
-import com.facebook.model.*;
+import android.view.View;
+import android.widget.Button;
 
-public class FaceBookLoginActivity extends Activity {
 
+public class MenuActivity extends Activity  {
+
+    Button btnTakePic;
+    Button btnWhatsThere;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_face_book_login);
+        setContentView(R.layout.activity_menu);
+
+        btnTakePic = (Button)findViewById(R.id.btnTakePic);
+        btnWhatsThere = (Button)findViewById(R.id.btnWhatsThere);
+
+        btnTakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.face_book_login, menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
@@ -35,16 +47,5 @@ public class FaceBookLoginActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-        Intent intent = new Intent();
-        intent.setClass(this,LocationService.class);
-        startActivity(intent);
-
     }
 }
